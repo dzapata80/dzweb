cecb7e1072433c62dfe0b36b94b397701ad37048
Daniel Zapata <d_zapata@hotmail.com>
2022-10-25 15:58:27 -0500

    V1.1.1

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+
+
+
+export default function  Navbar(){
+  return <nav className="nav">
+    <h1><a href="/" className="site-title">DZ</a></h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/proyects" >Proyects</a>
+      </li>
+    </ul>
+  </nav>
+}
